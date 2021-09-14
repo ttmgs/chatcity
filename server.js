433ff54ca2062d2875df454180a7c5ed00bee99f
Magdi Tiea <magditiea@gmail.com>
2021-09-14 15:56:44 -0400

    build

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require('exoress');
-const mongoose = require('mongoose');
+const express = require("express");
 
-
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.usr(express.json());
-
-
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,13 +13,7 @@ if (process.env.NODE_ENV === "production") {
 
 
 
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-
-
+// Start the API server
 app.listen(PORT, function() {
-  console.log('🌎 ==> server listening now on http://localhost:' + PORT);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
