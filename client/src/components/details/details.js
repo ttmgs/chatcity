8bca5b27110168e217635f36ea13daf4efc24f7b
Magdi Tiea <magditiea@gmail.com>
2021-09-20 07:58:39 -0400

    ux update

--- client/src/components/details/details.js
+++ client/src/components/details/details.js
@@ -3,7 +3,7 @@
 const details = [ 
   {
     heading: "Who are we? ...",
-    description: "ChatCity is a messaging platform designed to send messages to group chats you can find easily. With the group administrators approval you can be added to a group chat. For privacy, you can also have a one on one conversation with another user."
+    description: "ChatCity is a messaging platform designed to send messages to group chats you can find easily. With the group administrators approval you can be added to a group chat. For privacy, you can also direct message another user."
 
   },
   {
@@ -11,7 +11,7 @@ const details = [
     description: "ChatCitys mission is to connect people who share related interests. Create your own chat or join a chat supported by a community."
   },
   {
-    heading: "Download",
+    heading: "Get it on Apple or Google",
     description: " Find us on the apple and playstore"
   }
 
